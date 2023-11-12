3efcac5bd482ce095c7dca960457d57a34558545
Kumachar <1019190564@qq.com>
2023-11-12 12:02:45 -0500

    Update Description

--- R/linear_regression.R
+++ R/linear_regression.R
@@ -3,7 +3,7 @@
 #'This function is designed to obtain the coefficient \eqn{\beta} from ridge regression
 #'\deqn{Y = X\beta + e}
 #'and minimize \eqn{||Y-X\beta||^2},
-#'where \eqn{\beta} served as a predetermined hyperparamter to shift the diagonals of moment matrix.
+#'where \eqn{\beta} served as a desired parameter of model.
 #'More information about this method could be found in references.
 #'@references
 #'Wikipedia contributors. (n.d.). Ridge regression. In Wikipedia. Retrieved [November, 11th, 2023], from https://en.wikipedia.org/wiki/Ridge_regression
@@ -11,6 +11,7 @@
 #'@param x is a matrix of input features, each row represents an observation
 #'@param y is a matrix of output labels, each row represents an observation
 #'@param intercept is a bool value indicates whether there is intercept in the model, default value is true
+#'@param scale is a bool value indicates whether scale the input X in the model, default value is false
 #'
 #'@return the matrix of parameters \eqn{\beta} forlinear regression
 #'

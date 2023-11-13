a9ee26697d579200c446365c8215ebb5d9b12920
Kumachar <1019190564@qq.com>
2023-11-13 09:22:50 -0500

    Update function

--- tests/testthat/test-linear_regression.R
+++ tests/testthat/test-linear_regression.R
@@ -1,3 +1,4 @@
+library(testthat)
 test_that("multiplication works", {
 
   expect_equal(Linear_regression(mtcars[,c('hp','wt')],mtcars[,c('mpg')]),
@@ -9,4 +10,6 @@ test_that("multiplication works", {
   expect_equal(Linear_regression(data.matrix(mtcars[,c('hp','wt')]),data.matrix(mtcars[,c('mpg')]),intercept = FALSE),
                lm(mpg~hp+wt-1, data = mtcars)$coefficients, tolerance = 1e-6)
 
+  expect_equal(Linear_regression(cbind(1,1,data.matrix(mtcars[,c('hp','wt')])),data.matrix(mtcars[,c('mpg')])),
+               NULL, tolerance = 1e-6)
 })

b7ff5399c78e85b274e5047eedf293556d768a1c
Kumachar <1019190564@qq.com>
2023-11-13 12:23:20 -0500

    Update Help Page

--- R/linear_prediction.R
+++ R/linear_prediction.R
@@ -3,9 +3,20 @@
 #'This function is designed to obtain the prediction of linear regression
 #'
 #'@param X is a matrix of input features, each row represents an observation
-#'@param b is nums of parameters you obtained from linear regression
-#'@param intercept is a bool value denotes if model contain an intercept
+#'@param b is nums of parameters you obtained from linear_regression
+#'@param intercept is a bool value indicating if model contains an intercept
 #'@return the prediction \eqn{X\beta} for linear regression
+#'@examples
+#' #Load example dataset
+#' data(mtcars)
+#'
+#' #Use horsepower&weight to predict miles per gallon
+#' X <- mtcars[, c("hp", "wt")]  # horsepower and weight
+#' y <- mtcars$mpg               # miles per gallon
+#'
+#' # Run the linear regression
+#' b <- Linear_regression(X, y)
+#' result <- linear_prediction(X,b,TRUE)
 #'
 #'@export
 

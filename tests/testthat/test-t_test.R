0ef1ede09005fdfdfa199e072fbd77aa922b4290
Kumachar <1019190564@qq.com>
2023-11-13 22:17:23 -0500

    Update t-test function

--- tests/testthat/test-t_test.R
+++ tests/testthat/test-t_test.R
@@ -13,4 +13,14 @@ test_that("multiplication works", {
   expect_equal(testresult$t_value, rsum$coefficients[,'t value'],tolerance = 1e-6)
   expect_equal(testresult$p_value, rsum$coefficients[,'Pr(>|t|)'],tolerance = 1e-6)
 
+  X <- cbind(1,X)
+  model1 <- linear_model(X,y)
+  rsum <- summary(model2)
+  testresult <- parameter_t_test(model1)
+
+  expect_equal(testresult$Estimate, rsum$coefficients[,'Estimate'],tolerance = 1e-6, ignore_attr = TRUE)
+  expect_equal(testresult$StdError, rsum$coefficients[,'Std. Error'],tolerance = 1e-6, ignore_attr = TRUE)
+  expect_equal(testresult$t_value, rsum$coefficients[,'t value'],tolerance = 1e-6, ignore_attr = TRUE)
+  expect_equal(testresult$p_value, rsum$coefficients[,'Pr(>|t|)'],tolerance = 1e-6, ignore_attr = TRUE)
+
 })

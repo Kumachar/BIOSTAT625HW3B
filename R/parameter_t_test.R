79b9213cc225d0e10b4ebbcce73efdfa15d0500e
Kumachar <1019190564@qq.com>
2023-11-14 16:08:25 -0500

    Update R2

--- R/parameter_t_test.R
+++ R/parameter_t_test.R
@@ -48,5 +48,8 @@ parameter_t_test <- function(model){
   f =(SSY - sum(model$residuals^2))/df1/(sum(model$residuals^2)/df2)
   pfvalue = pf(f, df1, df2,lower.tail = F)
 
-  return(list(Estimate =beta,StdError=std, t_value=t, pt_value=ptvalue, f_value=f, pf_value=pfvalue))
+  R2 = sum((fitted.values-mean(y))^2)/sum((y-mean(y))^2)
+  R2_adj = 1-sum(residuals^2)/(nrow(X)-model$rank)/(sum((y-mean(y))^2)/(nrow(X)-1))
+
+  return(list(Estimate =beta,StdError=std, t_value=t, pt_value=ptvalue, f_value=f, pf_value=pfvalue, R2 = R2, R2_adj = R2_adj))
 }
--- tests/testthat/test-t_test.R
+++ tests/testthat/test-t_test.R
@@ -8,13 +8,16 @@ test_that("multiplication works", {
   rsum <- summary(model2)
   testresult <- parameter_t_test(model1)
 
+  rsum$r.squared
+
   expect_equal(testresult$Estimate, rsum$coefficients[,'Estimate'],tolerance = 1e-6)
   expect_equal(testresult$StdError, rsum$coefficients[,'Std. Error'],tolerance = 1e-6)
   expect_equal(testresult$t_value, rsum$coefficients[,'t value'],tolerance = 1e-6)
   expect_equal(testresult$pt_value, rsum$coefficients[,'Pr(>|t|)'],tolerance = 1e-6)
   expect_equal(testresult$f_value, as.numeric(rsum$fstatistic[1]),tolerance = 1e-6)
   expect_equal(testresult$pf_value, as.numeric(pf(rsum$fstatistic[1], rsum$fstatistic[2],rsum$fstatistic[3], lower.tail = F)),tolerance = 1e-6)
-
+  expect_equal(testresult$R2, rsum$r.squared, tolerance = 1e-6)
+  expect_equal(testresult$R2_adj, rsum$adj.r.squared, tolerance = 1e-6)
   X <- cbind(1,X)
   model1 <- linear_model(X,y)
   rsum <- summary(model2)

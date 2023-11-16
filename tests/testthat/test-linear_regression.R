7256d29135db4297f747fe39f223e8fe0d2c8ba1
Kumachar <1019190564@qq.com>
2023-11-16 18:08:25 -0500

    Update rcpp

--- tests/testthat/test-linear_regression.R
+++ tests/testthat/test-linear_regression.R
@@ -1,8 +1,12 @@
 library(testthat)
 test_that("multiplication works", {
 
+
+
   expect_equal(Linear_regression(mtcars[,c('hp','wt')],mtcars[,c('mpg')]),
                lm(mpg~hp+wt, data = mtcars)$coefficients, tolerance = 1e-6)
+
+
 
   expect_equal(Linear_regression(data.matrix(mtcars[,c('hp','wt')]),data.matrix(mtcars[,c('mpg')])),
                lm(mpg~hp+wt, data = mtcars)$coefficients, tolerance = 1e-6)
@@ -12,4 +16,7 @@ test_that("multiplication works", {
 
   expect_equal(Linear_regression(cbind(1,1,data.matrix(mtcars[,c('hp','wt')])),data.matrix(mtcars[,c('mpg')])),
                NULL, tolerance = 1e-6)
+  #Rcpp
+  expect_equal(Linear_regression(mtcars[,c('hp','wt')],mtcars[,c('mpg')],rcpp = T),
+               lm(mpg~hp+wt, data = mtcars)$coefficients, tolerance = 1e-6, ignore_attr = TRUE)
 })
